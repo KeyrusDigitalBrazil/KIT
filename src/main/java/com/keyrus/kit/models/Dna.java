5c5bfbced805dcd4b5b3cbeb17ea3ebf10ec8d81
Luan Souza <luan.souza@keyrus.com.br>
2020-03-11 08:41:31 -0300

    add bloodType enum

--- src/main/java/com/keyrus/kit/models/Dna.java
+++ src/main/java/com/keyrus/kit/models/Dna.java
@@ -2,19 +2,19 @@ package com.keyrus.kit.models;
 
 public class Dna {
 
-    private Integer id;
+    private Long id;
     private String dna;
 
-    public Dna(Integer id, String dna) {
+    public Dna(Long id, String dna) {
         this.id = id;
         this.dna = dna;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
