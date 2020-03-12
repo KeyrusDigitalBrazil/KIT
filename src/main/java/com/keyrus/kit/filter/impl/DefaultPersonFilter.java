c788ec2869d3125bd69ed2d53c3ad42195d3d2ab
LeonardoCalado <leonardosilva.uba@hotmail.com>
2020-03-12 08:24:30 -0300

    Fix filter

--- src/main/java/com/keyrus/kit/filter/impl/DefaultPersonFilter.java
+++ src/main/java/com/keyrus/kit/filter/impl/DefaultPersonFilter.java
@@ -1,8 +1,74 @@
 package com.keyrus.kit.filter.impl;
 
-import com.keyrus.kit.filter.PersonService;
+import com.keyrus.kit.filter.PersonFilter;
+import com.keyrus.kit.models.Dna;
+import com.keyrus.kit.models.Patient;
+import com.keyrus.kit.models.enums.BloodType;
+import com.keyrus.kit.models.enums.Nationality;
 
-public class DefaultPersonService implements PersonService {
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class DefaultPersonFilter implements PersonFilter {
 
+    private List<Patient> patientList;
+    private List<Dna> dnaList;
+
+    @Override
+    public Patient getPersonByDoc(String doc) {
+        return patientList.stream().filter(patient -> patient.getDoc().equals(doc)).findFirst().get();
+    }
+
+    @Override
+    public Dna getDnaById(Long id) {
+        return dnaList.stream().filter(dna -> dna.getId() == id).findFirst().get();
+    }
+
+    @Override
+    public List<Patient> getInfected() {
+        return patientList.stream().filter(patient -> patient.getConfirmed() == Boolean.TRUE).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Patient> getInfectedByNationality(Nationality nationality) {
+        return patientList.stream().filter(patient -> (patient.getConfirmed() == Boolean.TRUE && patient.getNationality().equals(nationality))).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Patient> getSuspicious() {
+        return patientList.stream().filter(patient -> patient.getSuspicious() == Boolean.TRUE).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Patient> getSuspiciousByNationality(Nationality nationality) {
+        return null;
+    }
+
+    @Override
+    public List<Patient> getNotInfected() {
+        return null;
+    }
+
+    @Override
+    public List<Patient> getNotInfectedByNationality(Nationality nationality) {
+        return null;
+    }
+
+    @Override
+    public List<Patient> getPatientCombineByBlood(BloodType bloodType) {
+        return null;
+    }
+
+    @Override
+    public List<Patient> getPatientCombineByBloodAndNationality(BloodType bloodType, Nationality nationality) {
+        return null;
+    }
+
+    public void setPatientList(List<Patient> patientList) {
+        this.patientList = patientList;
+    }
+
+    public void setDnaList(List<Dna> dnaList) {
+        this.dnaList = dnaList;
+    }
 }
