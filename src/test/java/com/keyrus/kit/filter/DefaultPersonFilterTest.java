9b878041a157dcd7a7299f6a957eea37f5869579
LeonardoCalado <leonardosilva.uba@hotmail.com>
2020-03-22 22:18:05 -0300

    Added personFilter tests

--- src/test/java/com/keyrus/kit/filter/DefaultPersonFilterTest.java
+++ src/test/java/com/keyrus/kit/filter/DefaultPersonFilterTest.java
@@ -4,12 +4,12 @@ import com.keyrus.kit.builder.PatientBuilder;
 import com.keyrus.kit.data.PatientDnaData;
 import com.keyrus.kit.filter.impl.DefaultPersonFilter;
 import com.keyrus.kit.models.Patient;
-import com.keyrus.kit.utils.impl.DefaultMenuUtils;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.keyrus.kit.models.enums.BloodType.*;
 import static com.keyrus.kit.models.enums.Nationality.*;
+import static org.junit.Assert.*;
 
 
 public class DefaultPersonFilterTest {
@@ -18,11 +18,9 @@ public class DefaultPersonFilterTest {
     private DefaultPersonFilter defaultPersonFilterEmpty;
     private Patient patient;
     private PatientDnaData patientDnaData;
-    private DefaultMenuUtils defaultMenuUtils;
 
     @Before
-    public void setUp(){
-        defaultMenuUtils = new DefaultMenuUtils();
+    public void setUp() {
         defaultPersonFilter = new DefaultPersonFilter();
         defaultPersonFilterEmpty = new DefaultPersonFilter();
         patient = PatientBuilder.defaultValues();
@@ -32,73 +30,133 @@ public class DefaultPersonFilterTest {
     }
 
     @Test
-    public void getPersonByDocSucess(){
-        Assert.assertEquals(defaultPersonFilter.getPersonByDoc("08172367082").toString(), patient.toString());
+    public void getPersonByDocSuccess() {
+        assertEquals(defaultPersonFilter.getPersonByDoc("08172367082").toString(), patient.toString());
     }
 
     @Test
-    public void getPersonByDocFail(){
-        Assert.assertNull(defaultPersonFilter.getPersonByDoc("1111111"));
+    public void getPersonByDocFail() {
+        assertNull(defaultPersonFilter.getPersonByDoc("1111111"));
     }
 
 
     @Test
-    public void getDnaByIdSucess(){
-        Assert.assertEquals(defaultPersonFilter.getDnaById(1L).toString(),patientDnaData.toString());
+    public void getDnaByIdSuccess() {
+        assertEquals(defaultPersonFilter.getDnaById(1L).toString(), patientDnaData.toString());
     }
 
     @Test
-    public void getDnaByIdFail(){
-        Assert.assertNotNull(defaultPersonFilter.getDnaById(2L));
+    public void getDnaByIdFail() {
+        assertNotNull(defaultPersonFilter.getDnaById(2L));
     }
 
     @Test
-    public void getDnaByCodeSucess(){
-        Assert.assertEquals(defaultPersonFilter.getDnaByCode("3D8F825").toString(),patientDnaData.toString());
+    public void getDnaByCodeSuccess() {
+        assertEquals(defaultPersonFilter.getDnaByCode("3D8F825").toString(), patientDnaData.toString());
     }
 
     @Test
-    public void getDnaByCodeFail(){
-        Assert.assertNotNull(defaultPersonFilter.getDnaByCode("111111"));
+    public void getDnaByCodeFail() {
+        assertNotNull(defaultPersonFilter.getDnaByCode("111111"));
     }
 
     @Test
-    public void getInfectedSucess(){
-        Assert.assertTrue(defaultPersonFilter.getInfected().size() > 0);
+    public void getInfectedSuccess() {
+        assertTrue(defaultPersonFilter.getInfected().size() > 0);
     }
 
     @Test
-    public void getInfectedFail(){
-        Assert.assertTrue(defaultPersonFilterEmpty.getInfected().isEmpty());
+    public void getInfectedFail() {
+        assertTrue(defaultPersonFilterEmpty.getInfected().isEmpty());
     }
 
     @Test
-    public void getInfectedByNationalitySucess(){
-        Assert.assertTrue(defaultPersonFilter.getInfectedByNationality(CHN).size() > 0);
+    public void getInfectedByNationalitySuccess() {
+        assertTrue(defaultPersonFilter.getInfectedByNationality(CHN).size() > 0);
     }
 
     @Test
-    public void getInfectedByNationalityFail(){
-        Assert.assertTrue(defaultPersonFilter.getInfectedByNationality(CA).isEmpty());
+    public void getInfectedByNationalityFail() {
+        assertTrue(defaultPersonFilter.getInfectedByNationality(CA).isEmpty());
     }
 
     @Test
-    public void getSuspiciousSucess(){
-        Assert.assertTrue(defaultPersonFilter.getSuspicious().size() > 0);
+    public void getSuspiciousSuccess() {
+        assertTrue(defaultPersonFilter.getSuspicious().size() > 0);
     }
 
     @Test
-    public void getSuspiciousFail(){
-        Assert.assertTrue(defaultPersonFilterEmpty.getSuspicious().isEmpty());
+    public void getSuspiciousFail() {
+        assertTrue(defaultPersonFilterEmpty.getSuspicious().isEmpty());
     }
 
     @Test
-    public void cleanStringSucess(){
-        Assert.assertEquals("08172367082", defaultPersonFilter.cleanString("081.72..;3.//6\\70-82"));
+    public void cleanStringSucess() {
+        assertEquals("08172367082", defaultPersonFilter.cleanString("081.72..;3.//6\\70-82"));
     }
 
     @Test
-    public void cleanStringFail(){
-        Assert.assertNotEquals("08172367082", defaultPersonFilter.cleanString("081.723.670AAAAAA-82"));
+    public void cleanStringFail() {
+        assertNotEquals("08172367082", defaultPersonFilter.cleanString("081.723.670AAAAAA-82"));
+    }
+
+    @Test
+    public void getSuspiciousByNationalitySuccess() {
+        assertTrue(defaultPersonFilter.getSuspiciousByNationality(CHN).size() > 0);
+    }
+
+    @Test
+    public void getSuspiciousByNationalityFail() {
+        assertTrue(defaultPersonFilterEmpty.getSuspiciousByNationality(CHN).isEmpty());
+    }
+
+    @Test
+    public void getNotInfectedSuccess() {
+        assertTrue(defaultPersonFilter.getNotInfected().size() > 0);
+    }
+
+    @Test
+    public void getNotInfectedFail() {
+        assertTrue(defaultPersonFilterEmpty.getNotInfected().isEmpty());
+    }
+
+    @Test
+    public void getNotInfectedByNationalitySuccess() {
+        assertTrue(defaultPersonFilter.getNotInfectedByNationality(BRA).size() > 0);
+    }
+
+    @Test
+    public void getNotInfectedByNationalityFail() {
+        assertTrue(defaultPersonFilterEmpty.getNotInfectedByNationality(BRA).isEmpty());
+    }
+
+    @Test
+    public void getPatientCombineByBloodSuccess() {
+        assertTrue(defaultPersonFilter.getPatientCombineByBlood(A_NEGATIVE).size() > 0);
+    }
+
+    @Test
+    public void getPatientCombineByBloodFail() {
+        assertTrue(defaultPersonFilterEmpty.getPatientCombineByBlood(A_NEGATIVE).isEmpty());
+    }
+
+    @Test
+    public void getPatientCombineByBloodAndNationalitySuccess() {
+        assertTrue(defaultPersonFilter.getPatientCombineByBloodAndNationality(A_NEGATIVE, BRA).size() > 0);
+    }
+
+    @Test
+    public void getPatientCombineByBloodAndNationalityFail() {
+        assertTrue(defaultPersonFilterEmpty.getPatientCombineByBloodAndNationality(A_NEGATIVE, BRA).isEmpty());
+    }
+
+    @Test
+    public void getPatientAllSuccess() {
+        assertTrue(defaultPersonFilter.getPatientAll().size() > 0);
+    }
+
+    @Test
+    public void getPatientAllFail() {
+        assertTrue(defaultPersonFilterEmpty.getPatientAll().isEmpty());
     }
 }
